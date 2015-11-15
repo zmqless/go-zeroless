8bf8b2281b223192deee90148394727d26fbe873
Lucas Lira Gomes <x8lucas8x@gmail.com>
2015-11-15 17:58:01 -0300

    Renamed some tests and increased the coverage for port is out of range checks.

--- exceptions_test.go
+++ exceptions_test.go
@@ -2,29 +2,53 @@ package zeroless
 
 import "testing"
 
-func TestPortUnderRange(t *testing.T) {
+func TestPortBellowRange(t *testing.T) {
 	port := 1023
 	_, err := NewServer(port).Push()
 
 	if err == nil {
-		t.Error("Port", port, "is under 1024")
+		t.Error("Port", port, "is bellow 1024")
+	}
+
+	client := NewClient()
+	client.ConnectLocal(port)
+	_, err = client.Push()
+
+	if err == nil {
+		t.Error("Port", port, "is bellow 1024")
 	}
 }
 
-func TestPortOnRange(t *testing.T) {
+func TestPortWithinRange(t *testing.T) {
 	port := 1024
 	_, err := NewServer(port).Push()
 
 	if err != nil {
-		t.Error("Port", port, "is not on range")
+		t.Error("Port", port, "is within range")
+	}
+
+	client := NewClient()
+	client.ConnectLocal(port)
+	_, err = client.Push()
+
+	if err != nil {
+		t.Error("Port", port, "is within range")
 	}
 }
 
-func TestPortAfterRange(t *testing.T) {
+func TestPortAboveRange(t *testing.T) {
 	port := 65536
 	_, err := NewServer(port).Push()
 
 	if err == nil {
-		t.Error("Port", port, "is after 65535")
+		t.Error("Port", port, "is above 65535")
+	}
+
+	client := NewClient()
+	client.ConnectLocal(port)
+	_, err = client.Push()
+
+	if err == nil {
+		t.Error("Port", port, "is above 65535")
 	}
 }
